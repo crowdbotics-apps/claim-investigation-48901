8cd735b617e42f007582aa4910283a902b210aac
Crowdbotics <team@crowdbotics.com>
2024-07-30 13:13:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "claim-investigation-48901",
+  url: "https://claim-investigation-48901.botics.co",
+  api: "https://claim-investigation-48901.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
